a44967f950854164b4ce475eb7676269fded3372
IVERTON EMIQUISON <ivertonbessa@gmail.com>
2024-04-19 01:18:38 -0300

    projeto_manutenção

---
+++ feiralivre/app/api/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from app.models import Verduras, Frutas, Entrega, Pagamento, Item
+
+class VerdurasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Verduras
+        fields = ['nome', 'preco_por_kg', 'estoque_kg']
+
+class FrutasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Frutas
+        fields = ['nome', 'preco_por_kg', 'quantidade_disponivel']
+
+class EntregaSerializer(serializers.ModelSerializer):
+    itens = serializers.PrimaryKeyRelatedField(queryset=Item.objects.all())
+    pagamento = serializers.PrimaryKeyRelatedField(queryset=Pagamento.objects.all())
+
+    class Meta:
+        model = Entrega
+        fields = ['nome_cliente', 'endereco_entrega', 'data_entrega', 'itens', 'pagamento']
+
+class PagamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pagamento
+        fields = ['metodo_pagamento', 'valor_total']
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    entrega = serializers.PrimaryKeyRelatedField(queryset=Entrega.objects.all())
+
+    class Meta:
+        model = Item
+        fields = ['nome_item', 'quantidade', 'preco_unitario', 'entrega']
+        
